bd920df63b298d268c52cd414cac70d31d469aa7
Altran DRA <altrandra@gmail.com>
2019-12-26 10:19:49 -0500

    fixes

--- diam/dict/default.go
+++ diam/dict/default.go
@@ -20,10 +20,10 @@ func init() {
 		{"Base", baseXML},
 		{"Credit Control", creditcontrolXML},
 		{"Gx Charging Control", gxcreditcontrolXML},
-		{"Network Access Server", networkaccessserverXML},
+		//{"Network Access Server", networkaccessserverXML},
 		{"TGPP", tgpprorfXML},
 		{"TGPP_S6a", tgpps6aXML},
-		{"TGPP_Swx", tgppswxXML},
+		//{"TGPP_Swx", tgppswxXML},
 		{"Accounting Control", accountingcontrolXML},
 	}
 	var err error
